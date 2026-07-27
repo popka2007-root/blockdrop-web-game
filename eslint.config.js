6244d8b58e1844822f8eade0772b3238bacc0e70
popka2007-root <ps4prosti@gmail.com>
2026-07-27 12:47:16 +0400

    Fix CI failure by rolling back faulty server.js refactor

--- eslint.config.js
+++ eslint.config.js
@@ -64,7 +64,7 @@ module.exports = [
       "playwright-report/**",
       "test-results/**",
       "screenshots/**",
-      "android/**"
+      "android/**",
     ],
   },
   js.configs.recommended,
